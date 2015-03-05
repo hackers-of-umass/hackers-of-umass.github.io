ba4260d5857f47484a09cfcc9d4e283fb19813af
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2015-03-04 22:58:56 -0500

    site v1 done

--- js/calendar.js
+++ js/calendar.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
+    $('#full-calendar').fullCalendar({
         googleCalendarApiKey: 'AIzaSyCiuiLNoFX1sBHzWOQ2b2zx0TrIQV4aHhw',
         eventSources: [
             {
                 googleCalendarId: 'hackersofumass@gmail.com'
             }
-        ]
+        ],
+        header: {
+            left: 'title',
+            center: '',
+            right: 'prev,next'
+        }
     });
 });
