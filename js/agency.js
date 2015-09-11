72930895643f7b3bbf67a090bdbe1517656d9ad6
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2015-09-11 19:14:40 -0400

    fix JS issues related to noConflict mode for jQuery

--- js/agency.js
+++ js/agency.js
@@ -5,7 +5,7 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+(function($) {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -13,25 +13,25 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    })
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
+    $('div.modal').on('show.bs.modal', function() {
+    	var modal = this;
+    	var hash = modal.id;
+    	window.location.hash = hash;
+    	window.onhashchange = function() {
+    		if (!location.hash){
+    			$(modal).modal('hide');
+    		}
+    	}
+    });
+})(jQuery);
--- js/calendar.js
+++ js/calendar.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-    $('#full-calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyCiuiLNoFX1sBHzWOQ2b2zx0TrIQV4aHhw',
-        eventSources: [
-            {
-                googleCalendarId: 'hackersofumass@gmail.com'
+(function($) {
+    $(document).ready(function() {
+        $('#full-calendar').fullCalendar({
+            googleCalendarApiKey: 'AIzaSyCiuiLNoFX1sBHzWOQ2b2zx0TrIQV4aHhw',
+            eventSources: [
+                {
+                    googleCalendarId: 'hackersofumass@gmail.com'
+                }
+            ],
+            header: {
+                left: 'title',
+                center: '',
+                right: 'prev,next'
             }
-        ],
-        header: {
-            left: 'title',
-            center: '',
-            right: 'prev,next'
-        }
+        });
     });
-});
+})(jQuery);
